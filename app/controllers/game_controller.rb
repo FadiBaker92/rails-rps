e61e0006cdde1da9ad6647497f4ebd75c800acc4
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-06-08 22:38:48 +0000

    added the logic to the gamecontroller. added html to play_rock.html.erb and the application.html.erb

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,15 @@
 class GameController < ApplicationController 
   def rock 
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+  
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else @comp_move == "scissors"
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_rock"})
   end
 end
